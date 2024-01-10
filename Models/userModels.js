adec424885d79bf1070323d753e401455d63527a
kobiowuquadri <kobiowuq@gmail.com>
2024-01-10 05:36:05 -0600

    connect to DB and create DB schema

---
+++ Models/userModels.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+
+// firstname, lastname, age, DOB, email
+const userSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    DOB: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+}, {timestamps : true})
+
+
+const userModel = mongoose.model('usermodels', userSchema)
+
+module.exports = userModel
+
--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
-
+const connectToDB  = require('./Database/db')
 dotenv.config()
 const app = express()
 
 const port = process.env.PORT
 
 // Nodemon -> helps update all changes & reload the server
+connectToDB()
 app.listen(port, () => {
     console.log(`Server is running on ${port}`)
 })
@@ -16,3 +17,8 @@ app.listen(port, () => {
 // PUT -> Update, 
 // DELETE -> Delete
 
+// Database Connection
+// Ralational Database || Non-Relational Databse
+// MONGODB -> NO-SQL Database || mongoose
+
+// Connect to DB
