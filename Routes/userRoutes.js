fe1a1ad7a973416e4cd4cb8f95eff2735949dd60
kobiowuquadri <kobiowuq@gmail.com>
2024-01-22 05:19:05 -0600

    add authorization logic

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const {registerUser, allUsers, updateUser, deleteUser, loginUser} = require('../Controllers/userControllers')
+const createTask = require('../Controllers/taskController')
+const verifyUser = require('../Middlewares/verify.js')
 const userRouters = express.Router()
 
 userRouters.post('/register-user', registerUser)
@@ -11,5 +13,7 @@ userRouters.put('/update-user/:id', updateUser)
 userRouters.put('/delete-user/:id', deleteUser)
 
 userRouters.post('/login-user', loginUser)
+
+userRouters.post('/add-task', verifyUser, createTask)
 
 module.exports = userRouters
